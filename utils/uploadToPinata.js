5e0d646efa8dff98cbbc11b845c36714bc548f53
Adeyemi Oluwagbogo <israeladeyemi489@gmail.com>
2023-06-11 13:20:09 +0100

    finisheduploadtopinanta

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,10 +1,30 @@
-const pinataSDK =  require("@pinata/sdk")
-const path = require("path")
-const fs = require("fs")
+const pinataSDK = require("@pinata/sdk");
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config()
 
-async function storeImages(imagesFilePath){
-    const fullImagesPath = path.resolve(imagesFilePath)
-    const files = fs.readdirSync(fullImagesPath)
-    console.log(files)
+const pinataApiKey = process.env.PINATA_API_KEY;
+const pinataApiSecret = process.env.PINATA_API_SECRET;
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret);
+
+async function storeImages(imagesFilePath) {
+  const fullImagesPath = path.resolve(imagesFilePath);
+  const files = fs.readdirSync(fullImagesPath);
+  console.log(files);
+  let responses = [];
+  console.log("Uploading to IPFS!")
+  for (fileIndex in files) {
+    console.log(`Working on ${fileIndex}...`)
+    const readableStreamForFile = fs.createReadStream(
+      `${fullImagesPath}/${files[fileIndex]}`
+    );
+    try {
+      const response = await pinata.pinFileToIPFS(readableStreamForFile);
+      responses.push(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return { responses, files };
 }
-module.exports = { storeImages}
+module.exports = { storeImages };
